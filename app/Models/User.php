a85329b7d054ba8e3ab324fe1cbc4338dbd13d36
dangvm-0369 <vu.minh.dang@sun-asterisk.com>
2023-08-28 10:23:46 +0700

    Chapter4: Accessors, Mutators and Scope

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,15 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Scopes\ActiveScope;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -51,5 +55,28 @@ class User extends Authenticatable
     public function roles(): BelongsToMany
     {
         return $this->belongsToMany(Role::class)->withTimestamps();
+    }
+
+    protected function fullName(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $this->attributes['first_name'] . ' ' . $this->attributes['last_name'],
+        );
+    }
+    protected function username(): Attribute
+    {
+        return Attribute::make(
+            set: fn($value) => Str::slug($value),
+        );
+    }
+
+    public function scopeAdmin(Builder $query): void
+    {
+        $query->where('is_admin', '=', true);
+    }
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new ActiveScope);
     }
 }
