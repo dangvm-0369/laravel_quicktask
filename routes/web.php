ac763eb03589e307c967532a1834a754c12c9d4e
dangvm-0369 <vu.minh.dang@sun-asterisk.com>
2023-08-29 15:04:19 +0700

    Chapter 7: Middleware

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/users', UserController::class);
+Route::resource('/users', UserController::class)->middleware(['admin']);
 
 Route::controller(CarController::class)->group(function (){
    Route::prefix('/cars')->group(function (){
